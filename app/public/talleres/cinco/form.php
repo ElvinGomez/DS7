e07cf898be4e2dacadf22a7d5b013834d13ad38e
Elvin Gomez <elviingoomez@gmail.com>
2021-10-13 17:16:53 -0500

    css added

--- app/public/talleres/cinco/form.php
+++ app/public/talleres/cinco/form.php
@@ -1,22 +1,64 @@
 <?php
     session_start();
-    $user =  $_SESSION["usuario"];
-
-    if($user === '' || !$user) {
+    if(!isset($_SESSION["usuario"])) {
         header("Location: index.php");
+    } else {
+        $user = $_SESSION["usuario"];
     }
 ?>
 
-<body style="margin: 0; padding: 0">
-    <div style="display: flex; justify-content: flex-end">
-        <h1><?php echo $user ?></h1>
+<html>
+<head>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <title>LOGIN</title>
+</head>
+<body>
+
+<nav class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-blueGray-800">
+  <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none hidden bg-blueGray-800" id="example-collapse-navbar">
+      <ul class="flex flex-col lg:flex-row list-none lg:ml-auto items-center">
+        <li class="flex items-center">
+          <a class="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150" href="https://www.creative-tim.com/product/notus-js?ref=njs-index" target="_blank">
+            <i class="fas fa-user"></i> <?php echo $user ?>
+          </a>
+        </li>
+      </ul>
     </div>
-    <div>
-        <form method="post" action="result.php">
-            Nombre: <input type="text" name="name">
-            Apellido: <input type="text" name="lastname">
-            Sueldo bruto: <input type="text" name="salary">
-                <input type="submit">
-        </form>
-    </div>
+  </div>
+</nav>
+<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+	<div class="relative py-3 sm:max-w-xl sm:mx-auto">
+		<div
+			class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+		</div>
+		<div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+			<div class="max-w-md mx-auto">
+				<div>
+					<h1 class="text-2xl font-semibold">Ingresa los datos para deducir los descuentos.</h1>
+				</div>
+                <form method="POST" action="result.php" class="divide-y divide-gray-200">
+					<div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+						<div class="relative">
+							<input autocomplete="off" id="name" name="name" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Nombre" />
+							<label for="name" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Nombre</label>
+						</div>
+						<div class="relative">
+							<input autocomplete="off" id="lastname" name="lastname" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Apellido" />
+							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Apellido</label>
+						</div>
+						<div class="relative">
+							<input autocomplete="off" id="salary" name="salary" type="number" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Salario" />
+							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Salario</label>
+						</div>
+						<div class="relative">
+							<button type="submit" class="bg-blue-500 text-white rounded-md px-2 py-1">Calcular</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
