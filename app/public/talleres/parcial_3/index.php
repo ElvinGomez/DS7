d98656451eeea57b2b16c43d931382299559e445
Elvin Gomez <elviingoomez@gmail.com>
2021-12-14 15:21:14 -0500

    parcial

--- app/public/talleres/parcial_3/index.php
+++ app/public/talleres/parcial_3/index.php
@@ -138,7 +138,6 @@
                                 <from class="relative">
                                   <select name="show_more" id="tables_selection" />';
                                   foreach($results as $table){
-                                    //Print the table name out onto the page.
                                     echo '<option value="'.$table[0].'">'.$table[0].'</option>';
                                   }
 ?>
@@ -163,84 +162,8 @@
                                             <tr id="table_header">
                                             </tr>
                                             </thead>
-
                                             <tbody id="table_body">
-                                            <!-- <tr>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                                                /argon/
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                                                4,569
-                                                </td>
-                                                <td class="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                340
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                46,53%
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                                                /argon/index.html
-                                                </th>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3,985
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                319
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <i class="fas fa-arrow-down text-orange-500 mr-4"></i>
-                                                46,53%
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                                                /argon/charts.html
-                                                </th>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3,513
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                294
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <i class="fas fa-arrow-down text-orange-500 mr-4"></i>
-                                                36,49%
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                                                /argon/tables.html
-                                                </th>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                2,050
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                147
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <i class="fas fa-arrow-up text-emerald-500 mr-4"></i>
-                                                50,87%
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                                                /argon/profile.html
-                                                </th>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                1,795
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                190
-                                                </td>
-                                                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <i class="fas fa-arrow-down text-red-500 mr-4"></i>
-                                                46,53%
-                                                </td>
-                                            </tr> -->
                                             </tbody>
-
                                         </table>
                                         </div>
                                     </div>
--- app/public/talleres/parcial_3/js/script.js
+++ app/public/talleres/parcial_3/js/script.js
@@ -1,15 +1,10 @@
-$( document ).ready(function() {
-    // const SelectedTable = $( "#tables_selection" ).val();
-
-    
+$( document ).ready(function() {    
     $( "#SelectBtn" ).click(function() {
         let SelectedTableName = $( "#tables_selection option:selected" ).text();
         $.ajax({
             dataType: "json",
             url: `http://127.0.0.1/talleres/parcial_3/table_data.php?name=${SelectedTableName}`,
             success: function(data) {
-                // console.log(data)
-                // console.log();
                 $( "#table_header" ).empty();
                 const id = Object.keys(data)[0];
                 Object.keys(data[id]).forEach(key => {
@@ -18,22 +13,13 @@ $( document ).ready(function() {
                 })
                 $( "#table_body" ).empty();
                 Object.keys(data).forEach(id => {
-                    // console.log(id)
                     const tr = $("#table_body").append('<tr></tr>')
                     Object.keys(data[id]).forEach(key => {
                         console.log(data[id][key])
-                        tr.append(`
-                            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">${data[id][key]}
-                            </td>`)//.append(`<th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">${data[id][key]}</th>`)
-                        // $("#table_body").append(`<th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">${key}</th>`);
+                        tr.append(`<td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">${data[id][key]}</td>`)
                     })
                 })
-                // Object.keys(data).forEach(id => {
-                //     // console.log(id)
-                // })
             }
         });
     });
-    
-
 });
