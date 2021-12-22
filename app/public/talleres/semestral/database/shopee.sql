a567189e8c61e696bb34edbf348b1589c9d1c4e4
isabelmelik06 <isabelmelik06@gmail.com>
2021-12-22 14:22:18 -0500

    Update shopee.sql
    update BD products

--- app/public/talleres/semestral/database/shopee.sql
+++ app/public/talleres/semestral/database/shopee.sql
@@ -20,53 +20,99 @@ SET time_zone = "+00:00";
 
 --
 -- Database: `shopee`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `cart_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
-  `item_id` int(11) NOT NULL,
-  `item_brand` varchar(200) NOT NULL,
-  `item_name` varchar(255) NOT NULL,
-  `item_price` double(10,2) NOT NULL,
-  `item_image` varchar(255) NOT NULL,
-  `item_register` datetime DEFAULT NULL
+  `product_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `inventory_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `item_image` varchar(255) NOT NULL
+ 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+REATE TABLE `product_category` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+REATE TABLE `shooping_items` (
+  `cart_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `price` decimal() NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+REATE TABLE `shooping_cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+REATE TABLE `shooping_order` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `total` decimal() NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item_image`, `item_register`) VALUES
-(1, 'Samsung', 'Samsung Galaxy 10', 152.00, './assets/products/1.png', '2020-03-28 11:08:57'), -- NOW()
-(2, 'Redmi', 'Redmi Note 7', 122.00, './assets/products/2.png', '2020-03-28 11:08:57'),
-(3, 'Redmi', 'Redmi Note 6', 122.00, './assets/products/3.png', '2020-03-28 11:08:57'),
-(4, 'Redmi', 'Redmi Note 5', 122.00, './assets/products/4.png', '2020-03-28 11:08:57'),
-(5, 'Redmi', 'Redmi Note 4', 122.00, './assets/products/5.png', '2020-03-28 11:08:57'),
-(6, 'Redmi', 'Redmi Note 8', 122.00, './assets/products/6.png', '2020-03-28 11:08:57'),
-(7, 'Redmi', 'Redmi Note 9', 122.00, './assets/products/8.png', '2020-03-28 11:08:57'),
-(8, 'Redmi', 'Redmi Note', 122.00, './assets/products/10.png', '2020-03-28 11:08:57'),
-(9, 'Samsung', 'Samsung Galaxy S6', 152.00, './assets/products/11.png', '2020-03-28 11:08:57'),
-(10, 'Samsung', 'Samsung Galaxy S7', 152.00, './assets/products/12.png', '2020-03-28 11:08:57'),
-(11, 'Apple', 'Apple iPhone 5', 152.00, './assets/products/13.png', '2020-03-28 11:08:57'),
-(12, 'Apple', 'Apple iPhone 6', 152.00, './assets/products/14.png', '2020-03-28 11:08:57'),
-(13, 'Apple', 'Apple iPhone 7', 152.00, './assets/products/15.png', '2020-03-28 11:08:57');
+INSERT INTO `product` (`product_id`, `name`, `category_id`, `inventory_id`, `price`, `quantity`) VALUES
+(1, 'Rose Mist Corporal', 'c1','001', 9.50','100'),
+(2, 'Twisted pepermint Mist Corporal', 'c1','002',9.00, '100'),
+(3, 'Honey Wildflowers Mist Corporal',  'c1','003',9.50, '100'),
+(4, 'Bath&body', 'Sunrise Woods Mist Corporal', 'c1','004',9.50, '100'),
+(5, 'Bath&body', 'Champagne Toast Mist corporal','c1','005',9.50, '100'),
+(6, 'Bath&body', 'Winter Candy apple Crema corporal','c1','006',9.50, '100'),
+(7, 'Bath&body', 'Open Sky Crema corporal', 'c1','007',9.50, '100'),
+(8, 'Bath&body', 'Pure Wonder Crema corporal', 'c1','008',9.50, '100'),
+(9, 'Bath&body', 'Bourbon Crema Corporal', 'c1','009',9.50, '100'),
+(10, 'Bath&body', 'Belize tropical cabana Crema corporal', 'c1','010',9.50, '100'),
+(11, 'Bath&body', 'Vanilla Bean Noel Shower gel', 'c1','011',9.50, '100'),
+(12, 'Bath&body', 'Warm vanilla sugar Shower gel ', 'c1','012',9.50, '100'),
+(13, 'Bath&body', 'Perfect peony Shower gel','c1','013',9.50, '100')
+(14, 'Bath&body', 'Japanese cherry blossom Shower gel ', 'c1','014',9.50, '100'),
+(15, 'Bath&body', 'Into the night Shower gel ', 'c1','015',9.50, '100'),
+(16, 'Bath&body', 'Chamomile Locion corporal ', 'c1','016',9.50, '100'),
+(17, 'Bath&body', 'Guava orange Locion corporal','c1','017',9.50, '100'),
+(18, 'Bath&body', 'Sunshine mimosa Locion corporal', 'c1','018',9.50, '100'),
+(19, 'Bath&body', 'Fiji sunshine guava-tini Locion corporal ', 'c1','019',9.50, '100'),
+(20, 'Bath&body', 'Hello Beautiful Locion corporal ', 'c1','020',9.50, '100'),
+(21, 'Bath&body', 'Fresh Balsam Vela 3 mechas', 'c2','021',27.00, '100'),
+(22, 'Bath&body', 'A thosand wishes Vela 3 mechas ', 'c2','022',27.00, '100'),
+(23, 'Bath&body', 'Limoncello Vela 3 mechas ', 'c2','023',27.00, '100'),
+(24, 'Bath&body', 'Pumping bonfire Vela 3 mechas ', 'c2','024',27.00, '100'),
+(25, 'Bath&body', 'Lakeside morning Vela 3 mechas', 'c2','025',27.00, '100'),
+(26, 'Bath&body', 'Eucalyptus spearmint Fragancia para wallflores ', 'c3','026',8.50, '100'),
+(27, 'Bath&body', 'Black Cherry merlot Fragancia para wallflores', 'c3','027',8.50, '100'),
+(28, 'Bath&body', 'A thosand wishes Fragancia para wallflores','c3','028',8.50, '100'),
+(29, 'Bath&body', 'Party dress Fragancia para wallflores ', 'c3','029',8.50, '100'),
+(30, 'Bath&body', 'Pink fairy gumdrop Fragancia para wallflores ', 'c3','030',8.50, '100'),
+(31, 'Bath&body', 'Twisted peppermint Jabon de gel ', 'c4','031',8.50, '100'),
+(32, 'Bath&body', 'White tea & sage Jabon de gel ', 'c4','032',8.50, '100'),
+(33, 'Bath&body', 'Frozen lake Jabon de gel ', 'c4','033',8.50, '100'),
+(34, 'Bath&body', 'Marshmallow fireside Jabon de gel ', 'c4','034',8.50, '100'),
+(35, 'Bath&body', 'Ornament Jabon de gel', 'c4','035',8.50, '100'),
+(36, 'Bath&body', 'Crushed candy cane Antibacterial', 'c4','036',8.50, '100'),
+(37, 'Bath&body', 'Winter citrus wreath Antibacterial ', 'c5','037',2.50, '100'),
+(38, 'Bath&body', 'Soft lavander Antibacterial ','c5','038',2.50, '100'),
+(39, 'Bath&body', 'Lemon and mint Antibacterial ', 'c5','039',2.50, '100'),
+(40, 'Bath&body', 'Flannel Gel antibacterial ', 'c5','040',2.50, '100'),
+(41, 'Bath&body', 'Crisp morning air Spray antibacterial', 'c5','041',7.00, '100'),
+(42, 'Bath&body', 'Fresh Rainfall Gel antibacterial ', 'c5','042',6.00, '100'),
+(43, 'Bath&body', 'Blue Ocean waves Antibacterial en espuma ', 'c5','043',6.00, '100'),
+(44, 'Bath&body', 'Happy BDAY to you Antibacterial en espuma ', 'c5','044',6.00, '100'),
+(45, 'Bath&body', 'Honolulu sun Antibacterial en espuma', 'c5','045',6.00, '100'),
+(46, 'Bath&body', 'Vanilla vean Noel Vela 1 mecha', 'c6','046',16.00, '100'),
+(47, 'Bath&body', 'Flannel Fragancia para wallflowers', 'c6','047',8.50, '100'),
+;
 
 -- --------------------------------------------------------
 
@@ -76,18 +122,47 @@ INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
-  `register_date` datetime DEFAULT NULL
+  `password` varchar(16) NOT NULL,
+  `location` varchar(60) NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `role_ref` int(11) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+CREATE TABLE `user_role` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(15) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user_address` (
+  `address_id` int(11) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  `address_line1` varchar(100) NOT NULL,
+  `address_line2` varchar(100) NOT NULL,
+  `city` varchar(16) NOT NULL,
+  `postal_code` varchar(60) NOT NULL,
+  `country` varchar(12) NOT NULL,
+  `telephone` varchar(11) NOT NULL,
+  `mobile` varchar(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user_payment` (
+  `payment_id` int(11) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  `payment_type` varchar(100) NOT NULL,
+  `address_line1` varchar(100) NOT NULL,
+  `city` varchar(16) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `register_date`) VALUES
-(1, 'Daily', 'Tuition', '2020-03-28 13:07:17'),
-(2, 'Akshay', 'Kashyap', '2020-03-28 13:07:17');
+(1, 'Daily', 'Tuition),
+(2, 'Akshay', 'Kashyap');
 
 -- --------------------------------------------------------
 
